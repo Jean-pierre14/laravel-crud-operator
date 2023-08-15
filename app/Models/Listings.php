15c67dd62ce593c0f81caf98e7f8ebbb97141b0a
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-08-15 18:44:19 +0100

    add: html pages

--- app/Models/Listings.php
+++ app/Models/Listings.php
@@ -1,30 +1,11 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    class Listings{
-        public static function all(){
-            return[
-                [
-                    'id' => 1, 
-                    'title' => 'Listing One',
-                    'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic vero eaque accusamus similique aliquid fugiat, repellat ea laboriosam molestias. Nemo rerum inventore accusamus amet commodi aperiam provident id, exercitationem tempore?'
-                ],
-                [
-                    'id' => 2, 
-                    'title' => 'Listing Two',
-                    'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic vero eaque accusamus similique aliquid fugiat, repellat ea laboriosam molestias. Nemo rerum inventore accusamus amet commodi aperiam provident id, exercitationem tempore?'
-                ]
-                ];
-        }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-        public static function find($id){
-            $listings = self::all();
-
-            foreach($listings as $listing){
-                if($listing['id'] == $id){
-                    return $listing;
-                }
-            }
-        }
-    }
+class Listings extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listings;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        Listings::factory(12)->create();
+        // Listings::create([
+        //     'title'=> 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email'=> 'email@example.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur dolor at aliquid nam, cumque vel eos numquam accusamus ullam autem corporis laudantium aspernatur, ex explicabo molestiae velit natus ab vitae!'
+        // ]);
+        // Listings::create([
+        //     'title'=> 'Full-stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email'=> 'email23@example.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur dolor at aliquid nam, cumque vel eos numquam accusamus ullam autem corporis laudantium aspernatur, ex explicabo molestiae velit natus ab vitae!'
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
